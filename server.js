07cefaf591fae7ad254b216ad35bef0d260e1a2f
Scott Wattenbarger <scottwattenbarger@gmail.com>
2023-04-13 13:29:14 -0700

    changes local host

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: process.env.REACT_APP_URL || 'http://localhost:3000',
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
